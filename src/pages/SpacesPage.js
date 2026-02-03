7d5d32cfc97fced180ce18c9f6e6c7a1c8a89cb2
Ephrahim Anyanje <atwoli2345@gmail.com>
2026-02-03 11:48:03 +0300

    Add basic SpacesPage component shell
    - Create SpacesPage component with basic wrapper structure
    - Initial scaffold for the spaces browsing page

---
+++ src/pages/SpacesPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const SpacesPage = () => {
+  return (
+    <div className="spaces-page-wrapper">
+      <div className="spaces-page">
+        <h1>Browse Spaces</h1>
+      </div>
+    </div>
+  );
+};
+
+export default SpacesPage;
