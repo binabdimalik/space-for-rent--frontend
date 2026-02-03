b166e12d3f8a7e5904c0597f790637db5688e0af
Ephrahim Anyanje <atwoli2345@gmail.com>
2026-02-03 11:41:15 +0300

    Add PrivacyPolicy.js basic structure with imports and empty sections array

---
+++ src/pages/PrivacyPolicy.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { FiShield, FiLock, FiEye, FiDatabase, FiMail } from "react-icons/fi";
+import Footer from "../components/common/Footer";
+
+const PrivacyPage = () => {
+  const sections = [];
+
+  return (
+    <div style={{ background: "#f5f5f4", minHeight: "calc(100vh - 70px)" }}>
+      <Footer />
+    </div>
+  );
+};
+
+export default PrivacyPage;
